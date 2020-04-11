9e425c230ed1da2fb206868737884904e9ac434c
renaman <n.renaman@icloud.com>
2020-04-11 09:34:13 +0900

    [Start]5-2,3ログイ投稿機能作成

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '2179faf4afbf8339867957613f25ebc764a606899ba6bc424e133287b5d736e3dfcf6d1a95ffde00a47f962ca9172c2f53d9274b9b6446c63ed3de2be42c25a9'
